11cd89803296bb4e36dab1ef6afad18a62cd4707
Bilal-Rauf <balihk30@gmail.com>
2020-10-16 17:55:59 -0600

    On branch develop  Changes to be committed: new file:   components/my_DFF.vhd
    Created my_DFF from Onens example from Lecture 8 Page 17

---
+++ components/my_DFF.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all
+
+entity my_DFF is 
+    port (D, CLK   : in std_logic;
+          RST     : in std_logic;
+          Q       : out std_logic
+          );
+end my_DFF;
+
+architecture BEHAV of my_DFF is 
+begin
+    process(CLK)
+        begin 
+            if rising_edge(CLK) then 
+                if RST = '0' then
+                    Q <= '0';
+                else
+                    Q <= D;
+                end if;
+            end if;
+    end process;
+end BEHAV; 
+            
