a721d9c0a53c40af094cae5f43b302eb2bf127a3
Bilal-Rauf <balihk30@gmail.com>
2020-11-10 18:11:02 -0700

    Added an enable input according to donen's feedback	modified:   components/REGISTER_BUS16.vhd Changed synchronous reset to asynchronous reset and added an enable signal	modified:   components/my_DFF.vhd

--- components/my_DFF.vhd
+++ components/my_DFF.vhd
@@ -4,20 +4,21 @@ use ieee.std_logic_1164.all;
 entity my_DFF is 
     port (D, CLK   : in std_logic;
           RST     : in std_logic;
+          ENABLE : in std_logic;
           Q       : out std_logic
           );
 end my_DFF;
 
 architecture BEHAV of my_DFF is 
 begin
-    process(CLK)
+    process(CLK,RST)
         begin 
-            if rising_edge(CLK) then 
                 if RST = '0' then
                     Q <= '0';
-                else
-                    Q <= D;
+                elseif rising_edge(CLK) then
+                    if ENABLE = '0' then
+                        Q <= D;
+                    end if;
                 end if;
-            end if;
     end process;
 end BEHAV;
